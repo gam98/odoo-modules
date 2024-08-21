7411fa53f442d9e804cf14eeec36cab30a102aa8
Gabriel Alejandro Mamani <gmamani@tinkin.one>
2024-08-21 10:22:01 -0300

    add better version

--- tkn-reward-estimate/models/models.py
+++ tkn-reward-estimate/models/models.py
@@ -5,28 +5,17 @@ class LoyaltyReward(models.Model):
 
     # price = fields.Float(string="Price")
     percentage = fields.Float(string="Percentage")
-    # points_required = fields.Float(string="Points Required", compute="_compute_points_required")
+    add_points_method = fields.Selection([
+        ('manual', 'Manualmente'),
+        ('automatic', 'Automático'),
+    ], string="¿Cómo desea agregar los puntos?", default='manual')
+    is_promotional = fields.Boolean(string="¿Es promocional?")
 
-    @api.onchange('percentage', 'gift_product_id')
+    @api.onchange('add_points_method', 'percentage', 'gift_product_id', 'is_promotional')
     def _onchange_calculate_point_cost(self):
-        for reward in self:
-            if reward.gift_product_id and reward.percentage:
-                # Suponiendo que el costo del producto es el precio de venta
-                # print('reward.gift_product_id -> ',reward.gift_product_id.standard_price)
-                # print('reward.gift_product_id -> ',reward.gift_product_id.list_price)
-                # print('reward.gift_product_id -> ',reward.gift_product_id.categ_id.name)
-
-                if(reward.gift_product_id.categ_id.name == 'Promocionales'):
-                  reward.point_cost = (reward.gift_product_id.standard_price / reward.percentage) * 100
-                else:
-                  reward.point_cost = (reward.gift_product_id.list_price / reward.percentage) * 100
-            else:
-                reward.point_cost = 0.0
-
-    # @api.depends('price', 'percentage')
-    # def _compute_points_required(self):
-    #     for reward in self:
-    #         if reward.price and reward.percentage:
-    #             reward.points_required = reward.price * (reward.percentage / 100)
-    #         else:
-    #             reward.points_required = 0.0
+      for reward in self:
+          if reward.add_points_method == 'automatic' and reward.gift_product_id and reward.percentage:
+              price = reward.gift_product_id.standard_price if reward.is_promotional else reward.gift_product_id.list_price
+              reward.point_cost = (price / reward.percentage) * 100
+          else:
+              reward.point_cost = 0.0 if reward.add_points_method == 'automatic' else reward.point_cost
