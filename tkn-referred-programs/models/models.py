85a3340cac0be661d5e5034fdfb64c79e78a8d55
gastonodetti <gastonodetti@gmail.com>
2024-09-13 11:39:02 -0300

    updates

--- tkn-referred-programs/models/models.py
+++ tkn-referred-programs/models/models.py
@@ -1,18 +1,13 @@
-# -*- coding: utf-8 -*-
+# from odoo import models, api
 
-# from odoo import models, fields, api
+# class PosOrder(models.Model):
+#     _inherit = 'pos.order'
 
-
-# class tkn-referred-programs(models.Model):
-#     _name = 'tkn-referred-programs.tkn-referred-programs'
-#     _description = 'tkn-referred-programs.tkn-referred-programs'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+#     def create_from_ui(self, orders, draft=False):
+#         # pylint: disable=no-member
+#         res = super(PosOrder, self).create_from_ui(orders, draft)
+#         for order in orders:
+#             existing_order = self.env['pos.order'].search(['|', ('id', '=', order['data'].get('server_id')), ('pos_reference', '=', order['data']['name'])], limit=1)
+#             if existing_order:
+#                 print('existing_order: ', existing_order)
+#         return res
